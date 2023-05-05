f50190170338aebce1efbd0dd13ef083f5207a99
Vikas <vikas1618072@gmail.com>
2023-05-05 12:30:17 +0530

    ready to deployment

--- video_transcription_project/settings.py
+++ video_transcription_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#z_6g*e=x)(^m9^@$tnrdam-!1+40+&=h2-&dpv=bk3t&4@_^8'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://video-transcript.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
